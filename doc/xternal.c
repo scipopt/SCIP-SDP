cefc70d32ff3a1323e00a3599d2c7eb32921836e
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2014-03-10 16:07:27 +0100

    added doxygen files

---
+++ doc/xternal.c
@@ -0,0 +1,44 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the                                 */
+/*      SDP-Package for SCIP: a solving framework for                        */
+/*                            mixed-integer semidefinite programms           */
+/*                                                                           */
+/* Copyright (C) 2011-2014 Discrete Optimization, TU Darmstadt               */
+/*                         EDOM, FAU Erlangen-Nürnberg                       */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU Lesser General Public License        */
+/* as published by the Free Software Foundation; either version 3            */
+/* of the License, or (at your option) any later version.                    */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.*/
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   xternal.c
+ * @brief  main document page
+ * @author Tristan Gally
+ */
+
+/*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+/**@mainpage Overview
+ *
+ * @version 1.0
+ * @author Sonja Mars, Lars Schewe, Tristan Gally, Marc Pfetsch
+ * @date 2011-2014
+ *
+ * The SCIP-SDP-Package is a plug-in for the Software SCIP. It is able to solve Mixed-Integer SDPs using the
+ * interior-point solver DSDP and SCIP as a branch and bound framework. It provides a lot of data handling, some
+ * presolve routines and an linear approximation procedure for MISDPs. Two examples for mixed-integer semidefinite
+ * programs are arising from truss topology design and maximum cut problems on graphs. We tested our code on a lot of
+ * instances, most of them are trusses or max-cuts.
+ */
