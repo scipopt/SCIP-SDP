32a13f2020bf0ec81c75abcbef37b600b91c4a88
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2014-04-09 14:11:51 +0200

    change to c-style

--- src/scipsdp/objrelax_sdp.h
+++ src/scipsdp/objrelax_sdp.h
@@ -30,71 +30,32 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/**@file   objrelax_sdp.h
- * @brief  sdp-relaxator
+/**@file   relax_sdp.h
+ * @ingroup RELAXATORS
+ * @brief  SDP relaxator
  * @author Sonja Mars
+ * @author Tristan Gally
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#ifndef __SCIP_OBJRELAXSDP_H__
-#define __SCIP_OBJRELAXSDP_H__
+#ifndef __SCIP_RELAXSDP_H__
+#define __SCIP_RELAXSDP_H__
 
-#include "objscip/objrelax.h"           // for ObjRelax
-//#include "scip/def.h"                   // for SCIP_Real
-//#include "scip/type_relax.h"            // for SCIP_RELAX
-//#include "scip/type_result.h"           // for SCIP_RESULT
-//#include "scip/type_retcode.h"          // for SCIP_RETCODE
-//#include "scip/type_scip.h"             // for SCIP
 #include "scip/scip.h"
 
-namespace scip
-{
-   /**class of the objective sdp-relaxator*/
-   class ObjRelaxSdp : public ObjRelax
-   {
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-   public:
-      /*lint --e{1540}*/
+/** creates the SDP relaxator and includes it in SCIP */
+EXTERN
+SCIP_RETCODE SCIPincludeRelaxSDP(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
 
-      /** default constructor */
-      ObjRelaxSdp(SCIP * scip) ;
-
-      /** destructor */
-      virtual ~ObjRelaxSdp() {}
-
-
-
-      /** execution method of relaxator
-       *
-       *  The method is called in the node processing loop. It solves the current subproblem's relaxation.
-       *  Like the LP relaxation, the relaxator should only operate on COLUMN variables.
-       *
-       *  possible return values for *result (if more than one applies, the first in the list should be used):
-       *  - SCIP_CUTOFF     : the node is infeasible in the variable's bounds and can be cut off
-       *  - SCIP_CONSADDED  : an additional constraint was generated, and the relaxator should not be called again on the
-       *                      same relaxation
-       *  - SCIP_REDUCEDDOM : a variable's domain was reduced, and the relaxator should not be called again on the same
-       *                      relaxation
-       *  - SCIP_SEPARATED  : a cutting plane was generated, and the relaxator should not be called again on the same
-       *                      relaxation
-       *  - SCIP_SUCCESS    : the relaxator solved the relaxation and should not be called again on the same relaxation
-       *  - SCIP_SUSPENDED  : the relaxator interrupted its solving process to wait for additional input (e.g. cutting
-       *                      planes); however, it is able to continue the solving in order to improve the dual bound
-       *  - SCIP_DIDNOTRUN  : the relaxator was skipped
-       */
-      virtual SCIP_RETCODE scip_exec(
-         SCIP*              scip,               /**< SCIP data structure */
-         SCIP_RELAX*        relax,              /**< the relaxator itself */
-         SCIP_Real*         lowerbound,         /**< pointer to store a lowerbound for the current node */
-         SCIP_RESULT*       result              /**< pointer to store the result of the relaxation call */
-         );
-
-
-   };
-
-} /* namespace scip */
-
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif
