e9edc456b469ee65f28eaf33070106f309cf8140
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2015-01-18 13:23:49 +0100

    fixed author

--- src/sdpi/type_sdpi.h
+++ src/sdpi/type_sdpi.h
@@ -15,7 +15,7 @@
 
 /**@file   type_sdpi.h
  * @brief  type definitions for specific SDP solvers interface
- * @author Tobias Achterberg
+ * @author Tristan Gally
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
