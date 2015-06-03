9511c33b706a42e6321b65d7caa072e5d0a01dbf
Tristan Gally <gally@mathematik.tu-darmstadt.de>
2015-06-03 14:20:38 +0200

    fixed indentation and add question marks again

--- src/scipsdp/branch_sdpobjective.c
+++ src/scipsdp/branch_sdpobjective.c
@@ -50,10 +50,10 @@
 #define BRANCHRULE_PRIORITY        0
 #define BRANCHRULE_MAXDEPTH        -1
 #define BRANCHRULE_MAXBOUNDDIST    1.0
-#define DEFAULT_COUPLEDVARS        FALSE /**< if all branching candidates have objective zero, should we use the sum of the absolute objectives of all
-                                          *  continuous variables coupled with the candidate through constraints */
-#define DEFAULT_SINGLECOUPLEDVARS  FALSE /**< if all branching candidates have objective zero, should we use the sum of the absolute objectives of all
-                                          *  continuous variables coupled with the candidate through constraints in which no other candidate appears */
+#define DEFAULT_COUPLEDVARS        FALSE     /**< if all branching candidates have objective zero, should we use the sum of the absolute objectives of all
+                                               *  continuous variables coupled with the candidate through constraints ? */
+#define DEFAULT_SINGLECOUPLEDVARS  FALSE     /**< if all branching candidates have objective zero, should we use the sum of the absolute objectives of all
+                                               *  continuous variables coupled with the candidate through constraints in which no other candidate appears ? */
 
 /** branching rule data */
 struct SCIP_BranchruleData
