79f6e18f2c404bfebdafa8049f74d665c0c4438c
Tristan Gally <gally@mathematik.tu-darmstadt.de>
2015-09-22 09:16:18 +0200

    changed sdpfrac to only run once in the root node
    this seems to produce the best solving times

--- src/scipsdp/heur_sdpfracdiving.c
+++ src/scipsdp/heur_sdpfracdiving.c
@@ -51,7 +51,7 @@
 #define HEUR_DESC             "SDP diving heuristic that chooses fixings w.r.t. the fractionalities"
 #define HEUR_DISPCHAR         'f'
 #define HEUR_PRIORITY         -1003000
-#define HEUR_FREQ             10
+#define HEUR_FREQ             0
 #define HEUR_FREQOFS          0
 #define HEUR_MAXDEPTH         -1
 #define HEUR_TIMING           SCIP_HEURTIMING_AFTERNODE
