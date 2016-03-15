15f545d1dc0810f61cedaf1ffab999e066e2ae5e
Tristan Gally <gally@mathematik.tu-darmstadt.de>
2016-03-15 09:03:42 +0100

    combined soling times into a single table, also removed mathcal for N(0,1)

--- RIP-MISDP-Paper/ResultScript.py
+++ RIP-MISDP-Paper/ResultScript.py
@@ -11,7 +11,8 @@ Lbounds = 0
 Rbounds = 0
 LRbounds = 0
 Ltimes = 0
-Rtimes = 1
+Rtimes = 0
+LRtimes = 1
 completeTable = 0
 
 MISDPfilename = "/local/gally/results/RIP-MISDP-Paper/160311/RIP-results/check.RIPMISDP.scipsdp.linux.x86_64.gnu.opt.sdpa.extra.branchinfobj_nofracdive.out"
@@ -206,7 +207,7 @@ def makeCompleteTableCaptionMISDP(shortcaption, caption, label, file, i):
 def LhsResultsTable(instancesets, instancesetnames, caption, label):
 	file.write("\\begin{table} \n \\begin{scriptsize} \\caption{" + caption + "} \n \\label{" + label + "} \n \\begin{tabular*}{\\linewidth}{@{}l@{\\;\\;\extracolsep{\\fill}}rr")
 	file.write("@{}}\\toprule \n")
-	file.write(" type of matrix & d'Asp 07 & d'Asp 08 ")
+	file.write(" matrices & d'Asp 07 & d'Asp 08 ")
 	file.write("\\\ \midrule \n")
 	totalDiff07 = 0
 	totalnInstances07 = 0
@@ -272,7 +273,7 @@ def LhsResultsTable(instancesets, instancesetnames, caption, label):
 def RhsResultsTable(instancesets, instancesetnames, caption, label):
 	file.write("\\begin{table} \n \\begin{scriptsize} \\caption{" + caption + "} \n \\label{" + label + "} \n \\begin{tabular*}{\\linewidth}{@{}l@{\\;\\;\extracolsep{\\fill}}rr")
 	file.write("@{}}\\toprule \n")
-	file.write(" type of matrix & d'Asp 07 & d'Asp 08 ")
+	file.write(" matrices & d'Asp 07 & d'Asp 08 ")
 	file.write("\\\ \midrule \n")
 	totalDiff07 = 0
 	totalnInstances07 = 0
@@ -340,7 +341,7 @@ def LhsRhsResultsTable(instancesets, instancesetnames, caption, label):
 	file.write("@{}}\\toprule \n")
 	file.write("  & \\multicolumn{2}{c}{$\\alpha_k$} & \\multicolumn{2}{c}{$\\beta_k$} \\\ \n")
 	file.write("\\cmidrule(r){2-3} \\cmidrule(l){4-5} \n")
-	file.write("type of matrix & d'Asp07 & d'Asp08 & d'Asp07 & d'Asp08 \\ \n")
+	file.write("matrices & d'Asp07 & d'Asp08 & d'Asp07 & d'Asp08 \\ \n")
 	file.write("\\\ \midrule \n")
 	totalDiff07l = 0
 	totalnInstances07l = 0
@@ -454,9 +455,9 @@ def LhsRhsResultsTable(instancesets, instancesetnames, caption, label):
 		
 
 def TimeTable(instancesets, instancesetnames, caption, label):
-	file.write("\\begin{table} \n \\begin{scriptsize} \\caption{" + caption + "} \n \\label{" + label + "} \n \\begin{tabular*}{\\linewidth}{@{}l@{\\;\\;\extracolsep{\\fill}}rrrr")
+	file.write("\\begin{table} \n \\begin{scriptsize} \\caption{" + caption + "} \n \\label{" + label + "} \n \\begin{tabular*}{\\linewidth}{@{}l@{\\;\\;\extracolsep{\\fill}}rrr")
 	file.write("@{}}\\toprule \n")
-	file.write(" type of matrix & MISDP & d'Asp 07 & d'Asp 08 ")
+	file.write(" matrices & MISDP & d'Asp 07 & d'Asp 08 ")
 	file.write("\\\ \midrule \n")
 	totalMISDPtime = 1.0
 	totalMISDPnum = 0
@@ -497,6 +498,85 @@ def TimeTable(instancesets, instancesetnames, caption, label):
 	file.write("total & \\num{%.1f" % totalMISDPtime + "} & " + "\\num{%.1f" % totalA07time + "} & " + "\\num{%.1f" % totalA08time + "} \\\ \n ")
 	file.write("\\bottomrule \n \\end{tabular*} \n \end{scriptsize} \n \\end{table} \n")
 
+def LhsRhsTimeTable(instancesets, instancesetnames, caption, label):
+	file.write("\\begin{table} \n \\begin{scriptsize} \\caption{" + caption + "} \n \\label{" + label + "} \n \\begin{tabular*}{\\linewidth}{@{}l@{\\;\\;\extracolsep{\\fill}}rrrrrr")
+	file.write("@{}}\\toprule \n")
+	file.write("  & \\multicolumn{3}{c}{$\\alpha_k$} & \\multicolumn{3}{c}{$\\beta_k$} \\\ \n")
+	file.write("\\cmidrule(r){2-4} \\cmidrule(l){5-7} \n")
+	file.write(" matrices & MISDP & d'Asp 07 & d'Asp 08 & MISDP & d'Asp 07 & d'Asp 08 ")
+	file.write("\\\ \midrule \n")
+	totalMISDPtimel = 1.0
+	totalMISDPnuml = 0
+	totalA07timel = 1.0
+	totalA07numl = 0
+	totalA08timel = 1.0
+	totalA08numl = 0
+	totalMISDPtimer = 1.0
+	totalMISDPnumr = 0
+	totalA07timer = 1.0
+	totalA07numr = 0
+	totalA08timer = 1.0
+	totalA08numr = 0
+	i = 0
+	for instancetype in instancesets:
+		MISDPtimel = 1.0
+		MISDPnuml = 0
+		A07timel = 1.0
+		A07numl = 0
+		A08timel = 1.0
+		A08numl = 0
+		MISDPtimer = 1.0
+		MISDPnumr = 0
+		A07timer = 1.0
+		A07numr = 0
+		A08timer = 1.0
+		A08numr = 0
+		for j1,j2 in instancetype:
+			MISDPtimel = math.pow(MISDPtimel, float(MISDPnuml) / float(MISDPnuml+1)) * math.pow(float(times[0][j1] + timeshift), 1.0/float(MISDPnuml+1))
+			MISDPnuml += 1
+			totalMISDPtimel = math.pow(totalMISDPtimel, float(totalMISDPnuml) / float(totalMISDPnuml+1)) * math.pow(float(times[0][j1] + timeshift), 1.0/float(totalMISDPnuml+1))
+			totalMISDPnuml += 1
+			A07timel = math.pow(A07timel, float(A07numl) / float(A07numl+1)) * math.pow(float(times[1][j1] + timeshift), 1.0/float(A07numl+1))
+			A07numl += 1
+			totalA07timel = math.pow(totalA07timel, float(totalA07numl) / float(totalA07numl+1)) * math.pow(float(times[1][j1] + timeshift), 1.0/float(totalA07numl+1))
+			totalA07numl += 1
+			A08timel = math.pow(A08timel, float(A08numl) / float(A08numl+1)) * math.pow(float(times[2][j1] + timeshift), 1.0/float(A08numl+1))
+			A08numl += 1
+			totalA08timel = math.pow(totalA08timel, float(totalA08numl) / float(totalA08numl+1)) * math.pow(float(times[2][j1] + timeshift), 1.0/float(totalA08numl+1))
+			totalA08numl += 1
+			MISDPtimer = math.pow(MISDPtimer, float(MISDPnumr) / float(MISDPnumr+1)) * math.pow(float(times[0][j2] + timeshift), 1.0/float(MISDPnumr+1))
+			MISDPnumr += 1
+			totalMISDPtimer = math.pow(totalMISDPtimer, float(totalMISDPnumr) / float(totalMISDPnumr+1)) * math.pow(float(times[0][j2] + timeshift), 1.0/float(totalMISDPnumr+1))
+			totalMISDPnumr += 1
+			A07timer = math.pow(A07timer, float(A07numr) / float(A07numr+1)) * math.pow(float(times[1][j2] + timeshift), 1.0/float(A07numr+1))
+			A07numr += 1
+			totalA07timer = math.pow(totalA07timer, float(totalA07numr) / float(totalA07numr+1)) * math.pow(float(times[1][j2] + timeshift), 1.0/float(totalA07numr+1))
+			totalA07numr += 1
+			A08timer = math.pow(A08timer, float(A08numr) / float(A08numr+1)) * math.pow(float(times[2][j2] + timeshift), 1.0/float(A08numr+1))
+			A08numr += 1
+			totalA08timer = math.pow(totalA08timer, float(totalA08numr) / float(totalA08numr+1)) * math.pow(float(times[2][j2] + timeshift), 1.0/float(totalA08numr+1))
+			totalA08numr += 1
+		MISDPtimel = MISDPtimel - timeshift
+		A07timel = A07timel - timeshift
+		A08timel = A08timel - timeshift
+		MISDPtimer = MISDPtimer - timeshift
+		A07timer = A07timer - timeshift
+		A08timer = A08timer - timeshift
+		file.write(instancesetnames[i] + "& \\num{%.1f" % MISDPtimel + "} & " + "\\num{%.1f" % A07timel + "} & " + "\\num{%.1f" % A08timel + "} ")
+		file.write("& \\num{%.1f" % MISDPtimer + "} & " + "\\num{%.1f" % A07timer + "} & " + "\\num{%.1f" % A08timer + "} \\\ \n ")
+		i += 1
+	file.write("\\midrule \n")
+	totalMISDPtimel = totalMISDPtimel - timeshift
+	totalA07timel = totalA07timel - timeshift
+	totalA08timel = totalA08timel - timeshift
+	totalMISDPtimer = totalMISDPtimer - timeshift
+	totalA07timer = totalA07timer - timeshift
+	totalA08timer = totalA08timer - timeshift
+	file.write("total & \\num{%.1f" % totalMISDPtimel + "} & " + "\\num{%.1f" % totalA07timel + "} & " + "\\num{%.1f" % totalA08timel + "} ")
+	file.write("& \\num{%.1f" % totalMISDPtimer + "} & " + "\\num{%.1f" % totalA07timer + "} & " + "\\num{%.1f" % totalA08timer + "} \\\ \n ")
+	file.write("\\bottomrule \n \\end{tabular*} \n \end{scriptsize} \n \\end{table} \n")
+
+
 if __name__=="__main__":
 	"""give any number of .out-files for the same testset, then loops over them and returns a .tex-file given as first argument with some tables and a performance graph """
 	ninstances=[0 for x in range(len(sys.argv) -2)] #initialize ninstances matrix
@@ -526,19 +606,21 @@ if __name__=="__main__":
 		file.write("\\documentclass[landscape]{article} \n \\usepackage{amsmath} \n \\usepackage{amsthm} \n \\usepackage{dsfont} \n \\usepackage[dvipsnames]{xcolor} \n \\usepackage{booktabs} \n \\usepackage{multirow} \n \\usepackage{mathtools} \n \\usepackage{xtab} \n \usepackage{tikz} \n \usepackage{pgfplots} \n  \usepackage[margin=1in,footskip=0.25in]{geometry} \n \\extrafloats{100} \n \\usepackage{sistyle} \n \\usepackage{tabularx} \n \\newcommand{\\setting}[1]{\\texttt{#1}} \n \\begin{document} \n")
 
 	if Lbounds:
-		LhsResultsTable([[54,56,58,60,62,64,66,68,70],[18,20,22,24,26,28,30,32,34],[90,92,94,96,98,100,102,104,106],[72,74,76,78,80,82,84,86,88],[36,38,40,42,44,46,48,50,52],[0,2,4,6,8,10,12,14,16]], ["$\\mathcal{N}(0,1)$", "binary", "band matrix", "$\\mathcal{N}(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average gap of relaxations for RIC $\\alpha_k$", "lhsGap")
+		LhsResultsTable([[54,56,58,60,62,64,66,68,70],[18,20,22,24,26,28,30,32,34],[90,92,94,96,98,100,102,104,106],[72,74,76,78,80,82,84,86,88],[36,38,40,42,44,46,48,50,52],[0,2,4,6,8,10,12,14,16]], ["$N(0,1)$", "binary", "band matrix", "$N(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average gap of relaxations for RIC $\\alpha_k$", "lhsGap")
 
 	if Rbounds:
-		RhsResultsTable([[55,57,59,61,63,65,67,69,71],[19,21,23,25,27,29,31,33,35],[91,93,95,97,99,101,103,105,107],[109,111,113,115,117,119,121,123,125],[73,75,77,79,81,83,85,87,89],[37,39,41,43,45,47,49,51,53],[1,3,5,7,9,11,13,15,17]], ["$\\mathcal{N}(0,1)$", "binary", "band matrix", "rank 1", "$\\mathcal{N}(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average gap of relaxations for RIC $\\beta_k$", "rhsGap")
+		RhsResultsTable([[55,57,59,61,63,65,67,69,71],[19,21,23,25,27,29,31,33,35],[91,93,95,97,99,101,103,105,107],[109,111,113,115,117,119,121,123,125],[73,75,77,79,81,83,85,87,89],[37,39,41,43,45,47,49,51,53],[1,3,5,7,9,11,13,15,17]], ["$N(0,1)$", "binary", "band matrix", "rank 1", "$N(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average gap of relaxations for RIC $\\beta_k$", "rhsGap")
 
 	if LRbounds:
-		LhsRhsResultsTable([[[54,55],[56,57],[58,59],[60,61],[62,63],[64,65],[66,67],[68,69],[70,71]],[[18,19],[20,21],[22,23],[24,25],[26,27],[28,29],[30,31],[32,33],[34,35]],[[90,91],[92,93],[94,95],[96,97],[98,99],[100,101],[102,103],[104,105],[106,107]],[[108,109],[110,111],[112,113],[114,115],[116,117],[118,119],[120,121],[122,123],[124,125]],[[72,73],[74,75],[76,77],[78,79],[80,81],[82,83],[84,85],[86,87],[88,89]],[[36,37],[38,39],[40,41],[42,43],[44,45],[46,47],[48,49],[50,51],[52,53]],[[0,1],[2,3],[4,5],[6,7],[8,9],[10,11],[12,13],[14,15],[16,17]]], ["$\\mathcal{N}(0,1)$", "binary", "band matrix", "rank 1", "$\\mathcal{N}(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average gap of relaxations for RICs", "lhsRhsGap")
+		LhsRhsResultsTable([[[54,55],[56,57],[58,59],[60,61],[62,63],[64,65],[66,67],[68,69],[70,71]],[[18,19],[20,21],[22,23],[24,25],[26,27],[28,29],[30,31],[32,33],[34,35]],[[90,91],[92,93],[94,95],[96,97],[98,99],[100,101],[102,103],[104,105],[106,107]],[[108,109],[110,111],[112,113],[114,115],[116,117],[118,119],[120,121],[122,123],[124,125]],[[72,73],[74,75],[76,77],[78,79],[80,81],[82,83],[84,85],[86,87],[88,89]],[[36,37],[38,39],[40,41],[42,43],[44,45],[46,47],[48,49],[50,51],[52,53]],[[0,1],[2,3],[4,5],[6,7],[8,9],[10,11],[12,13],[14,15],[16,17]]], ["$N(0,1)$", "binary", "band matrix", "rank 1", "$N(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average gap of relaxations for RICs", "lhsRhsGap")
 
 	if Ltimes:
-		TimeTable([[54,56,58,60,62,64,66,68,70],[18,20,22,24,26,28,30,32,34],[90,92,94,96,98,100,102,104,106],[72,74,76,78,80,82,84,86,88],[36,38,40,42,44,46,48,50,52],[0,2,4,6,8,10,12,14,16]], ["$\\mathcal{N}(0,1)$", "binary", "band matrix", "$\\mathcal{N}(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Solving times for left-hand side of RIP", "lhsTime")
+		TimeTable([[54,56,58,60,62,64,66,68,70],[18,20,22,24,26,28,30,32,34],[90,92,94,96,98,100,102,104,106],[72,74,76,78,80,82,84,86,88],[36,38,40,42,44,46,48,50,52],[0,2,4,6,8,10,12,14,16]], ["$N(0,1)$", "binary", "band matrix", "$N(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average Solving times for left-hand side of RIP", "lhsTime")
 
 	if Rtimes:
-		TimeTable([[55,57,59,61,63,65,67,69,71],[19,21,23,25,27,29,31,33,35],[91,93,95,97,99,101,103,105,107],[109,111,113,115,117,119,121,123,125],[73,75,77,79,81,83,85,87,89],[37,39,41,43,45,47,49,51,53],[1,3,5,7,9,11,13,15,17]], ["$\\mathcal{N}(0,1)$", "binary", "band matrix", "rank 1", "$\\mathcal{N}(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Solving times for right-hand side of RIP", "rhsTime")
+		TimeTable([[55,57,59,61,63,65,67,69,71],[19,21,23,25,27,29,31,33,35],[91,93,95,97,99,101,103,105,107],[109,111,113,115,117,119,121,123,125],[73,75,77,79,81,83,85,87,89],[37,39,41,43,45,47,49,51,53],[1,3,5,7,9,11,13,15,17]], ["$N(0,1)$", "binary", "band matrix", "rank 1", "$N(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average Solving times for right-hand side of RIP", "rhsTime")
+	if LRtimes:
+		LhsRhsTimeTable([[[54,55],[56,57],[58,59],[60,61],[62,63],[64,65],[66,67],[68,69],[70,71]],[[18,19],[20,21],[22,23],[24,25],[26,27],[28,29],[30,31],[32,33],[34,35]],[[90,91],[92,93],[94,95],[96,97],[98,99],[100,101],[102,103],[104,105],[106,107]],[[108,109],[110,111],[112,113],[114,115],[116,117],[118,119],[120,121],[122,123],[124,125]],[[72,73],[74,75],[76,77],[78,79],[80,81],[82,83],[84,85],[86,87],[88,89]],[[36,37],[38,39],[40,41],[42,43],[44,45],[46,47],[48,49],[50,51],[52,53]],[[0,1],[2,3],[4,5],[6,7],[8,9],[10,11],[12,13],[14,15],[16,17]]], ["$N(0,1)$", "binary", "band matrix", "rank 1", "$N(0,1/m)$", "$\\pm 1/\\sqrt{m}$", "$0, \\pm \\sqrt{3/m}$"], "Average Solving times for RICs", "rhsTime")
 
 
 	if texfile:
