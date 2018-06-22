4ebd8e336c77150d7683b065396e29fa43b05c79
Tristan Gally <gally@mathematik.tu-darmstadt.de>
2018-06-22 10:05:58 +0200

    first (untested) cmake version

---
+++ cmake/Modules/FindDSDP.cmake
@@ -0,0 +1,14 @@
+find_path(DSDP_INCLUDE_DIRS
+    NAMES dsdp5.h
+    HINTS ${DSDP_DIR} $ENV{DSDP_DIR}
+    PATH_SUFFIXES include)
+
+find_library(DSDP_LIBRARY
+    NAMES dsdp
+    HINTS ${DSDP_DIR} $ENV{DSDP_DIR}
+    PATH_SUFFIXES lib)
+
+set(DSDP_LIBRARIES ${DSDP_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(DSDP DEFAULT_MSG DSDP_INCLUDE_DIRS DSDP_LIBRARIES)
