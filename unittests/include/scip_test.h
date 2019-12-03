193dc0309b636725af804eec49b63b4473f045d7
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2019-12-03 16:13:24 +0100

    add unittests

---
+++ unittests/include/scip_test.h
@@ -0,0 +1,81 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/* This file is part of SCIPSDP - a solving framework for mixed-integer      */
+/* semidefinite programs based on SCIP.                                      */
+/*                                                                           */
+/* Copyright (C) 2011-2013 Discrete Optimization, TU Darmstadt               */
+/*                         EDOM, FAU Erlangen-Nürnberg                       */
+/*               2014-2019 Discrete Optimization, TU Darmstadt               */
+/*                                                                           */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU Lesser General Public License        */
+/* as published by the Free Software Foundation; either version 3            */
+/* of the License, or (at your option) any later version.                    */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.*/
+/*                                                                           */
+/*                                                                           */
+/* Based on SCIP - Solving Constraint Integer Programs                       */
+/* Copyright (C) 2002-2019 Zuse Institute Berlin                             */
+/* SCIP is distributed under the terms of the SCIP Academic Licence,         */
+/* see file COPYING in the SCIP distribution.                                */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* This file is copied from SCIP. */
+
+#include "scip/scip.h"
+#include "locale.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
+#endif
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <criterion/parameterized.h>
+#include <criterion/theories.h>
+
+#ifdef __GNUC__
+#pragma GCC diagnostic warning "-Wredundant-decls"
+#pragma GCC diagnostic warning "-Wstrict-prototypes"
+#pragma GCC diagnostic warning "-Wshadow"
+#endif
+
+#undef SCIP_CALL
+#define SCIP_CALL(x)   do                                                                                     \
+                       {                                                                                      \
+                          SCIP_RETCODE _restat_;                                                              \
+                          if( (_restat_ = (x)) != SCIP_OKAY )                                                 \
+                          {                                                                                   \
+                             cr_assert(FALSE, "Error <%d> in function call\n", _restat_);                     \
+                          }                                                                                   \
+                       }                                                                                      \
+                       while( FALSE )
+
+
+CR_API int main(int argc, char *argv[])
+{
+   struct criterion_test_set* tests = criterion_initialize();
+   int result = 0;
+
+   if ( criterion_handle_args(argc, argv, true) )
+   {
+      setlocale(LC_ALL, "C");
+      result = !criterion_run_all_tests(tests);
+   }
+
+   criterion_finalize(tests);
+   return result;
+}
