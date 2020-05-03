a58c8ac27eb90e78cb8d0a3d7a825dd2e7b36450
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2020-05-03 15:15:25 +0200

    run heuristic even before presolving

--- src/scipsdp/heur_sdpinnerlp.c
+++ src/scipsdp/heur_sdpinnerlp.c
@@ -69,7 +69,7 @@
 #define HEUR_FREQ             0
 #define HEUR_FREQOFS          0
 #define HEUR_MAXDEPTH         -1
-#define HEUR_TIMING           SCIP_HEURTIMING_AFTERNODE
+#define HEUR_TIMING           SCIP_HEURTIMING_BEFOREPRESOL
 #define HEUR_USESSUBSCIP      TRUE  /* does the heuristic use a secondary SCIP instance? */
 
 
