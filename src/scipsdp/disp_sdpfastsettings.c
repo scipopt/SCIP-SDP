30386d13f47383341790edd5868393a8c7dd893a
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2020-08-20 13:47:49 +0200

    turn off display columns by default

--- src/scipsdp/disp_sdpfastsettings.c
+++ src/scipsdp/disp_sdpfastsettings.c
@@ -164,7 +164,7 @@ SCIP_RETCODE SCIPincludeDispSdpfastsettings(
    SCIP_CALL( SCIPallocMemory(scip, &dispdata) );
 
    /* include display column */
-   SCIP_CALL( SCIPincludeDisp(scip, DISP_NAME, DISP_DESC, DISP_HEADER, SCIP_DISPSTATUS_AUTO,
+   SCIP_CALL( SCIPincludeDisp(scip, DISP_NAME, DISP_DESC, DISP_HEADER, SCIP_DISPSTATUS_OFF,
          dispCopySdpfastsettings,
          dispFreeSdpfastsettings, NULL, NULL,
          dispInitsolSdpfastsettings, NULL, dispOutputSdpfastsettings,
