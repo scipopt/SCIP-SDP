e16314b523aba374314526a373e488cf6fd00993
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2020-12-13 12:47:00 +0100

    minor fix

--- src/sdpi/sdpi.c
+++ src/sdpi/sdpi.c
@@ -874,9 +874,9 @@ SCIP_RETCODE computeLpLhsRhsAfterFixings(
             }
 
             /* check whether this makes the problem infeasible */
-            if ( sdpi->sdpiub[lpcol] < sdpi->sdpilb[lpcol] - sdpi->epsilon )
+            if ( sdpiub[lpcol] < sdpilb[lpcol] - sdpi->epsilon )
             {
-               SCIPdebugMessage("Found upper bound %g < lower bound %g for variable %d -> infeasible!\n", sdpi->sdpiub[lpcol], sdpi->sdpilb[lpcol], lpcol);
+               SCIPdebugMessage("Found upper bound %g < lower bound %g for variable %d -> infeasible!\n", sdpiub[lpcol], sdpilb[lpcol], lpcol);
                sdpi->infeasible = TRUE;
                return SCIP_OKAY;
             }
