ad1b16cd48e4e47e0f8b2b0ea95eadbba84c2e61
Frederic Matter <matter@mathematik.tu-darmstadt.de>
2021-02-18 13:26:37 +0100

    extend unittest to also cover added SDP cons

--- unittests/src/newvars.c
+++ unittests/src/newvars.c
@@ -40,6 +40,7 @@
 #include <scip/cons_linear.h>
 #include "scipsdp/scipsdpdefplugins.h"
 #include "include/scip_test.h"
+#include "scipsdp/cons_sdp.h"
 
 /* global SCIP data structure */
 SCIP* scipsdp;
@@ -121,3 +122,97 @@ Test(newvars, addvars)
 
    cr_assert_float_eq(obj, trueobj, EPS, "Optimal values differ: %g != %g (true value)\n", obj, trueobj);
 }
+
+/** Test 2 */
+Test(newvars, addSDPcons)
+{
+   SCIP_CONS* newcons;
+   SCIP_CONS* newSDPcons;
+   SCIP_VAR* newvar1;
+   SCIP_VAR* newvar2;
+   SCIP_VAR* newvar3;
+   SCIP_Real* sdpvals[3];
+   SCIP_Real trueobj = -8.0;
+   SCIP_Real obj;
+   SCIP_Real constval = -1.0;
+   SCIP_VAR* vars[3];
+   SCIP_Real vals[3] = {1.0, 1.0, 1.0};
+   int nvarnonz[3] = {1, 1, 1};
+   int constcol = 0;
+   int constrow = 0;
+   int* cols[3];
+   int* rows[3];
+   int i;
+
+   /* read problem in primal form and solve it */
+   SCIP_CALL( SCIPreadProb(scipsdp, "../instances/example_small.dat-s", NULL) );
+
+   /* presolve */
+   SCIP_CALL( SCIPpresolve(scipsdp) );
+
+   /* add a new variable */
+   SCIP_CALL( SCIPcreateVarBasic(scipsdp, &newvar1, "newvar1", 0.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPaddVar(scipsdp, newvar1) );
+
+   SCIP_CALL( SCIPcreateVarBasic(scipsdp, &newvar2, "newvar2", 0.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPaddVar(scipsdp, newvar2) );
+
+   SCIP_CALL( SCIPcreateVarBasic(scipsdp, &newvar3, "newvar3", 0.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPaddVar(scipsdp, newvar3) );
+
+   vars[0] = newvar1;
+   vars[1] = newvar2;
+   vars[2] = newvar3;
+
+   SCIP_CALL( SCIPcreateConsBasicLinear(scipsdp, &newcons, "newcons", 3, vars, vals, 1.0, 1.0) );
+   SCIP_CALL( SCIPaddCons(scipsdp, newcons) );
+
+   for (i = 0; i < 3; i++)
+   {
+      SCIP_CALL( SCIPallocBufferArray(scipsdp, &cols[i], 1) );
+      SCIP_CALL( SCIPallocBufferArray(scipsdp, &rows[i], 1) );
+      SCIP_CALL( SCIPallocBufferArray(scipsdp, &sdpvals[i], 1) );
+   }
+
+   cols[0][0] = 0;
+   cols[1][0] = 0;
+   cols[2][0] = 1;
+
+   rows[0][0] = 0;
+   rows[1][0] = 1;
+   rows[2][0] = 1;
+
+   sdpvals[0][0] = 1.0;
+   sdpvals[1][0] = 1.0;
+   sdpvals[2][0] = 1.0;
+
+
+   SCIP_CALL( SCIPcreateConsSdp(scipsdp, &newSDPcons, "newSDPcons", 3, 3, 2, nvarnonz,
+         cols, rows, sdpvals, vars, 1, &constcol, &constrow, &constval, FALSE) );
+   SCIP_CALL( SCIPaddCons(scipsdp, newSDPcons) );
+
+   for (i = 0; i < 3; i++)
+   {
+      SCIPfreeBufferArray(scipsdp, &sdpvals[i]);
+      SCIPfreeBufferArray(scipsdp, &rows[i]);
+      SCIPfreeBufferArray(scipsdp, &cols[i]);
+   }
+
+   SCIP_CALL( SCIPreleaseVar(scipsdp, &newvar3) );
+   SCIP_CALL( SCIPreleaseVar(scipsdp, &newvar2) );
+   SCIP_CALL( SCIPreleaseVar(scipsdp, &newvar1) );
+   SCIP_CALL( SCIPreleaseCons(scipsdp, &newcons) );
+   SCIP_CALL( SCIPreleaseCons(scipsdp, &newSDPcons) );
+
+   /* presolve again */
+   SCIP_CALL( SCIPpresolve(scipsdp) );
+
+   /* solve */
+   SCIP_CALL( SCIPsolve(scipsdp) );
+
+   obj = SCIPgetDualbound(scipsdp);
+
+   cr_assert_float_eq(obj, trueobj, EPS, "Optimal values differ: %g != %g (true value)\n", obj, trueobj);
+}
+
+
