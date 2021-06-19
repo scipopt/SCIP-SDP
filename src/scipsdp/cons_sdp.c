fbd21b49e24f0b99b7c773e699cbd3937206b37c
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2021-06-19 10:47:58 +0200

    change parameter value to original value

--- src/scipsdp/cons_sdp.c
+++ src/scipsdp/cons_sdp.c
@@ -110,7 +110,7 @@
 #define DEFAULT_TWOMINORLINCONSS  FALSE /**< Should linear cuts corresponding to 2 by 2 minors be added? */
 #define DEFAULT_TWOMINORPRODCONSS FALSE /**< Should linear cuts corresponding to products of 2 by 2 minors be added? */
 #define DEFAULT_QUADCONSRANK1      TRUE /**< Should quadratic cons for 2x2 minors be added in the rank-1 case? */
-#define DEFAULT_UPGRADEQUADCONSS   TRUE /**< Should quadratic constraints be upgraded to a rank 1 SDP? */
+#define DEFAULT_UPGRADEQUADCONSS  FALSE /**< Should quadratic constraints be upgraded to a rank 1 SDP? */
 #define DEFAULT_UPGRADEKEEPQUAD   FALSE /**< Should the quadratic constraints be kept in the problem after upgrading and the corresponding SDP constraint be added without the rank 1 constraint? */
 #define DEFAULT_MAXNVARSQUADUPGD   1000 /**< maximal number of quadratic constraints and appearing variables so that the QUADCONSUPGD is performed */
 #define DEFAULT_RANK1APPROXHEUR   FALSE /**< Should the heuristic that computes the best rank-1 approximation for a given solution be executed? */
