ca6099cbc029176893c405485d6b66ba98a24a61
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2021-07-21 15:40:17 +0200

    use INT_MAX as iteration limit

--- src/sdpi/arpack_interface.c
+++ src/sdpi/arpack_interface.c
@@ -140,7 +140,7 @@ SCIP_RETCODE SCIParpackComputeSmallestEigenvector(
    NCV = 2;
    LDV = n;
    IPARAM[0] = 1;       /* exact shifts */
-   IPARAM[2] = 100000;  /* maximal number of iterations */
+   IPARAM[2] = INT_MAX; /* maximal number of iterations */
    IPARAM[6] = 1;       /* Mode 1: A*x = lambda*x, A symmetric, => OP = A  and  B = I. */
    LWORKL = NCV * (NCV + 8);  /* must be at least NCV**2 + 8*NCV */
    INFO = 0;          /* use random starting vector */
