ccdfae28b7898627d4d7728239a1d585236d223b
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2022-03-04 18:19:36 +0100

    output additional statistics

--- src/scipsdp/cons_sdp.c
+++ src/scipsdp/cons_sdp.c
@@ -134,6 +134,7 @@
 #define DEFAULT_USEDIMACSFEASTOL  FALSE /**< Should a feasibility tolerance based on the DIMACS be used for computing negative eigenvalues? */
 #define DEFAULT_GENERATEROWS       TRUE /**< Should rows be generated (constraints otherwise)? */
 #define DEFAULT_PRESOLLINCONSSPARAM   0 /**< Parameters for linear constraints added during presolving: (0) propagate, if solving LPs also separate (1) initial and propagate, if solving LPs also separate, enforce and check */
+#define DEFAULT_ADDITIONALSTATS    TRUE /**< Should additional statistics should be output at the end? */
 
 #ifdef OMP
 #define DEFAULT_NTHREADS              1 /**< number of threads used for OpenBLAS */
@@ -229,6 +230,10 @@ struct SCIP_ConshdlrData
    SCIP_Bool             recomputeinitial;   /**< Should the inital vector for TPower be computed each time before calling TPower (instead of using the original smallest eigenvector)? */
    SCIP_Bool             exacttrans;         /**< Should the matrix be transformed with the exact maximal eigenvalue before calling TPower (instead of using estimate)? */
    int                   presollinconssparam; /**< Parameters for linear constraints added during presolving: (0) propagate, if solving LPs also separate (1) initial and propagate, if solving LPs also separate, enforce and check */
+   SCIP_Bool             additionalstats;    /**< Should additional statistics should be output at the end? */
+   int                   npropub;            /**< Number of propagations through upper bounds */
+   int                   nproptb;            /**< Number of tightended bounds */
+   int                   nprop3minor;        /**< Number of propagations through 3x3 minors */
 };
 
 /** generates matrix in colum-first format (needed by LAPACK) from matrix given in full row-first format (SCIP-SDP
@@ -5918,6 +5923,18 @@ SCIP_DECL_CONSEXIT(consExitSdp)
    conshdlrdata = SCIPconshdlrGetData(conshdlr);
    assert(conshdlrdata != NULL);
 
+   /* possibly output more statistics */
+   if ( conshdlrdata->additionalstats )
+   {
+      SCIPverbMessage(scip, SCIP_VERBLEVEL_FULL, 0, "Number of propagations through upper bounds: %d\n", conshdlrdata->npropub);
+      SCIPverbMessage(scip, SCIP_VERBLEVEL_FULL, 0, "Number of tightened bounds in propagation:   %d\n", conshdlrdata->nproptb);
+      SCIPverbMessage(scip, SCIP_VERBLEVEL_FULL, 0, "Number of propagations through 3x3 minors:   %d\n", conshdlrdata->nprop3minor);
+   }
+   /* reset counters */
+   conshdlrdata->npropub = 0;
+   conshdlrdata->nproptb = 0;
+   conshdlrdata->nprop3minor = 0;
+
    /* reset parameter triedlinearconss */
    conshdlrdata->sdpconshdlrdata->triedlinearconss = FALSE;
    conshdlrdata->sdpconshdlrdata->triedvarbounds = FALSE;
@@ -6049,6 +6066,7 @@ SCIP_DECL_CONSPROP(consPropSdp)
       {
          if ( nprop > 0 )
          {
+            conshdlrdata->npropub += nprop;
             SCIPdebugMsg(scip, "Propagation tightened %d bounds.\n", nprop);
             *result = SCIP_REDUCEDDOM;
          }
@@ -6078,6 +6096,7 @@ SCIP_DECL_CONSPROP(consPropSdp)
          {
             if ( nprop > 0 )
             {
+               conshdlrdata->nproptb += nprop;
                SCIPdebugMsg(scip, "Propagation tightened %d bounds.\n", nprop);
                *result = SCIP_REDUCEDDOM;
             }
@@ -6105,6 +6124,7 @@ SCIP_DECL_CONSPROP(consPropSdp)
       {
          if ( nprop > 0 )
          {
+            conshdlrdata->nprop3minor += nprop;
             SCIPdebugMsg(scip, "Propagation of 3x3 minors tightened %d bounds.\n", nprop);
             *result = SCIP_REDUCEDDOM;
          }
@@ -8147,6 +8167,10 @@ SCIP_RETCODE SCIPincludeConshdlrSdp(
          "Parameters for linear constraints added during presolving: (0) propagate, if solving LPs also separate (1) initial and propagate, if solving LPs also separate, enforce and check",
          &(conshdlrdata->presollinconssparam), TRUE, DEFAULT_PRESOLLINCONSSPARAM, 0, 1, NULL, NULL) );
 
+   SCIP_CALL( SCIPaddBoolParam(scip, "constraints/SDP/additionalstats",
+         "Should additional statistics should be output at the end?",
+         &(conshdlrdata->additionalstats), TRUE, DEFAULT_ADDITIONALSTATS, NULL, NULL) );
+
    return SCIP_OKAY;
 }
 
@@ -8223,6 +8247,9 @@ SCIP_RETCODE SCIPincludeConshdlrSdpRank1(
    conshdlrdata->randnumgen = NULL;
    conshdlrdata->relaxsdp = NULL;
    conshdlrdata->dimacsfeastol = SCIP_INVALID;
+   conshdlrdata->npropub = 0;
+   conshdlrdata->nproptb = 0;
+   conshdlrdata->nprop3minor = 0;
 
    /* include constraint handler */
    SCIP_CALL( SCIPincludeConshdlrBasic(scip, &conshdlr, CONSHDLRRANK1_NAME, CONSHDLRRANK1_DESC,
